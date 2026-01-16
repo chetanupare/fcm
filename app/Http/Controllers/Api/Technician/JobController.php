0235284c31eb5c51558c8a7a330daf09e52c3653
Chetan Upare <chetan.upare@neosoftmail.com>
2026-01-17 04:39:06 +0530

    Allow direct transition from offered to en_route/component_pickup - auto-accept job

--- app/Http/Controllers/Api/Technician/JobController.php
+++ app/Http/Controllers/Api/Technician/JobController.php
@@ -466,7 +466,7 @@ class JobController extends Controller
     protected function getValidTransitions(string $currentStatus): array
     {
         $transitions = [
-            'offered' => ['accepted'],
+            'offered' => ['accepted', 'en_route', 'component_pickup'],
             'accepted' => ['en_route', 'component_pickup'],
             'en_route' => ['component_pickup', 'arrived'],
             'component_pickup' => ['arrived', 'en_route'],
