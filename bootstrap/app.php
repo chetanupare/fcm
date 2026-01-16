4506364a10f56c2fba8325e217d393f612eb8ac3
Chetan Upare <chetan.upare@neosoftmail.com>
2026-01-16 16:30:44 +0530

    Add CORS configuration and middleware to fix API CORS errors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\EnsureUserHasRole::class,
         ]);
+        
+        // Add CORS middleware globally
+        $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
